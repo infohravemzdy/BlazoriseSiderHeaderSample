c6fff8713280dbb64b4986abad32ee4b8d94979d
mzdyhrave <info@hravemzdy.org>
2022-05-24 13:14:58 +0200

    Initial commit

---
+++ Config.cs
@@ -0,0 +1,18 @@
+﻿using Blazorise;
+
+namespace BlazoriseSiderHeaderSample
+{
+    public static class Config
+    {
+        public static IServiceCollection SetupBlazorise(this IServiceCollection services)
+        {
+            services
+                .AddBlazorise(options =>
+                {
+                    options.Immediate = true;
+                });
+
+            return services;
+        }
+    }
+}
